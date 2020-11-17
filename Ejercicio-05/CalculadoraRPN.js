9fb277dd7e892506e5082bf08b93ef28be82914f
Alberto <uo266536@uniovi.es>
2020-11-17 11:56:14 +0100

    Cinco primeros ejercicios resueltos

--- Ejercicio-05/CalculadoraRPN.js
+++ Ejercicio-05/CalculadoraRPN.js
@@ -1,143 +1,186 @@
 "use strict";
-class CalculadoraPolaca{
-    constructor(){
-        this.numero="";
-        this.stack=new Stack();
+class PilaLIFO { 
+    constructor (nombre){ 
+        this.nombre = nombre;
+        this.pila = new Array();
     }
-
-    actualizar(){
-        
-        var p1 = document.getElementById("display2");
-        
-        p1.value = this.stack.peek();
-
-        var p2 = document.getElementById("display1");
-        
-       
-        p2.value = this.stack.doblePeek();
-
-        var p3 = document.getElementById("display3");
-       
-        p3.value=this.numero;
+    apilar(valor){
+        this.pila.push(valor);
     }
-    addDigit(numero){
-        this.numero=this.numero+numero;
+    desapilar(){
+        return (this.pila.pop());
+    }
+    mostrar(){
+        var stringPila = this.nombre;
+        for (let i = this.pila.length - 1 ; i >= 0; i--) {
+            var indexReal = i+1;
+            stringPila += "\nElemento " + indexReal + " : " + this.pila[i];
+        }
+        return stringPila;
+    }
+    reiniciarPila(){
+        this.pila = new Array();
+    }
+}
+
+class CalculadoraRPN {
+    constructor() {
+        this.pantalla = "";
+        this.memoria= "0";
         this.actualizar();
-    
     }
-    operar(operacion){
-        var result;
-        switch(operacion){
-            case '+':          
-            result=parseInt(this.stack.pop())+parseInt(this.stack.pop());
-            this.stack.push(result);
-            break;
-            case '-':          
-            result=this.stack.pop()-this.stack.pop();
-            this.stack.push(result);
-            break;
-            case '/':          
-            result=this.stack.pop()/this.stack.pop();
-            this.stack.push(result);
-            break;
-            case '*':          
-            result=this.stack.pop()*this.stack.pop();
-            this.stack.push(result);
-            break;
-            case '*':          
-            result=this.stack.pop()*this.stack.pop();
-            this.stack.push(result);
-            break;
-            case 'CE':          
-            this.stack.pop();  
-            break;
-            case 'C':          
-            this.stack=new Stack();
-            break;
-            case 'add':
-            if(this.numero===""){}else{
-            this.stack.push(this.numero);
-            this.numero="";
-            }
-            break;
-            case 'sen':          
-            this.stack.push(Math.sin(this.stack.pop()));
-            break;
-            case 'cos':          
-            this.stack.push(Math.cos(this.stack.pop()));
-            break;
-            case 'tan':          
-            this.stack.push(Math.tan(this.stack.pop()));
-            case 'exp':          
-            this.stack.push(Math.exp(this.stack.pop()));
-            break;
-            case 'raiz':          
-            this.stack.push(Math.sqrt(this.stack.pop()));
-            break;
-            case 'p2':  
-            break;        
-            this.stack.push(Math.pow(this.stack.pop(),2));
-            case 'log':          
-            this.stack.push(Math.log(this.stack.pop()));
-            break;
 
+    añadirAPila(){
+        try { 
+            pilaLifo.apilar(this.pantalla);
+            this.actualizar();
+            this.pantalla = "";
+            this.actualizar();
+        }
+        catch(err) {
+            document.getElementById('pantalla').value = "Error = " + err;
+        } 
+    }
+
+    actualizar() {
+        var display = document.getElementById('mostrarPila');
+        display.value = pilaLifo.mostrar();
+        var screen = document.getElementById('pantalla');
+        screen.value = this.pantalla;
+    }
+
+    borrar(){
+        this.pantalla = "";
+        this.actualizar();
+    }
+
+    dígitos(dígito){
+        this.pantalla += dígito;
+        this.actualizar();
+    }
+
+    realizarOperacion(operacion){
+        var resultadoOperacion;
+        switch(operacion){
+            case '+':
+                var operando1 = parseFloat(pilaLifo.desapilar());
+                var operando2 = parseFloat(pilaLifo.desapilar());
+                resultadoOperacion =  operando2 + operando1;
+                pilaLifo.apilar(resultadoOperacion);
+                break;
+            case '-':
+                var operando1 = parseFloat(pilaLifo.desapilar());
+                var operando2 = parseFloat(pilaLifo.desapilar());
+                resultadoOperacion =  operando2 - operando1;
+                pilaLifo.apilar(resultadoOperacion);
+                break;
+            case '/':
+                var operando1 = parseFloat(pilaLifo.desapilar());
+                var operando2 = parseFloat(pilaLifo.desapilar());
+                resultadoOperacion =  operando2 / operando1;
+                pilaLifo.apilar(resultadoOperacion);
+                break;
+            case '*':
+                var operando1 = parseFloat(pilaLifo.desapilar());
+                var operando2 = parseFloat(pilaLifo.desapilar());
+                resultadoOperacion =  operando2 * operando1;
+                pilaLifo.apilar(resultadoOperacion);
+                break;
+            case 'sin':
+                var operando1 = parseFloat(pilaLifo.desapilar());
+                resultadoOperacion =  Math.sin(operando1);
+                pilaLifo.apilar(resultadoOperacion);
+                break;
+            case 'cos':
+                var operando1 = parseFloat(pilaLifo.desapilar());
+                resultadoOperacion =  Math.cos(operando1);
+                pilaLifo.apilar(resultadoOperacion);
+                break;
+            case 'tan':
+                var operando1 = parseFloat(pilaLifo.desapilar());
+                resultadoOperacion =  Math.tan(operando1);
+                pilaLifo.apilar(resultadoOperacion);
+                break;
+            case 'log10':
+                var operando1 = parseFloat(pilaLifo.desapilar());
+                resultadoOperacion =  Math.log10(operando1);
+                pilaLifo.apilar(resultadoOperacion);
+                break;
+            case 'log2':
+                var operando1 = parseFloat(pilaLifo.desapilar());
+                resultadoOperacion =  Math.log2(operando1);
+                pilaLifo.apilar(resultadoOperacion);
+                break;
+            case 'exp':
+                var operando1 = parseFloat(pilaLifo.desapilar());
+                resultadoOperacion =  Math.exp(operando1);
+                pilaLifo.apilar(resultadoOperacion);
+                break;
+            case 'sqrt':
+                var operando1 = parseFloat(pilaLifo.desapilar());
+                resultadoOperacion =  Math.sqrt(operando1);
+                pilaLifo.apilar(resultadoOperacion);
+                break;
+            case 'cambioSigno':
+                var operando1 = parseFloat(pilaLifo.desapilar());
+                resultadoOperacion =  (-1)*operando1;
+                pilaLifo.apilar(resultadoOperacion);
+                break;
+            case 'cuadrado':
+                var operando1 = parseFloat(pilaLifo.desapilar());
+                resultadoOperacion =  Math.pow(operando1,2);
+                pilaLifo.apilar(resultadoOperacion);
+                break;
+            case 'elevarXaY':
+                var operando1 = parseFloat(pilaLifo.desapilar());
+                var operando2 = parseFloat(pilaLifo.desapilar());
+                resultadoOperacion =  Math.pow(operando2, operando1);
+                pilaLifo.apilar(resultadoOperacion);
+                break;
+            case 'elevarA10':
+                var operando1 = parseFloat(pilaLifo.desapilar());
+                resultadoOperacion =  Math.pow(10, operando1);
+                pilaLifo.apilar(resultadoOperacion);
+                break;
+            case 'factorial':
+                var operando1 = parseFloat(pilaLifo.desapilar());
+                resultadoOperacion =  this.factorialAux(operando1);
+                pilaLifo.apilar(resultadoOperacion);
+                break;
         }
         this.actualizar();
-
-
     }
 
-    
-   
+    factorialAux(num){
+        if (num === 0) { 
+            return 1; 
+        }
+        else { 
+            return num * this.factorialAux(num - 1); 
+        }
+    }
+    reiniciarPila(){
+        pilaLifo.reiniciarPila();
+        this.actualizar();
+    }
+    borrarUltimoNumero(){
+        pilaLifo.desapilar();
+        this.actualizar();
+    }
+    borrarUltimoCaracter(){
+        try{
+            this.pantalla = this.pantalla.substring(0, this.pantalla.length - 1);
+            this.actualizar();
+            }catch(exception){
+                this.pantalla=this.pantalla+"";
+                this.borrarUltimoCaracter();
+            }
+    }
+}
 
-    
-} 
 
-// Stack class 
-class Stack { 
+var pilaLifo = new PilaLIFO("Pila de la calculadora RPN: ");
+var calculadora = new CalculadoraRPN();
 
-	// Array is used to implement stack 
-	constructor() 
-	{ 
-		this.items = []; 
-	} 
 
-	
-push(element) 
-{ 
-	
-	this.items.push(element); 
-} 
 
-// pop function 
-pop() 
-{ 
-	
-	if (this.items.length == 0) 
-		return 0; 
-	return this.items.pop(); 
-} 
-
-	// peek function 
-peek() 
-{ 
-	// return the top most element from the stack 
-    // but does'nt delete it. 
-    if(this.items.length>=1)
-    return this.items[this.items.length - 1]; 
-    else return "---"; 
-} 
-doblePeek() 
-{ 
-	// return the top most element from the stack 
-    // but does'nt delete it. 
-    if(this.items.length>=2)
-    return this.items[this.items.length - 2];
-    else return "---"; 
-} 
- 
-	// isEmpty() 
-	// printStack() 
-} 
-
-var calculadora=new CalculadoraPolaca();
