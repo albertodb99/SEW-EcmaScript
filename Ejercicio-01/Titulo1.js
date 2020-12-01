28ffab2f7e663107cdf3c38f01a810e508764ddb
Alberto <uo266536@uniovi.es>
2020-12-01 10:38:56 +0100

    Catorce primeros ejercicios resueltos

--- Ejercicio-01/Titulo1.js
+++ Ejercicio-01/Titulo1.js
@@ -1,6 +1,6 @@
 //Titulo1.js
 //Archivo de ECMAScript correspondiente a
 //la tarea 4 del Ejercicio 1.
-document.write("<h1>");
+document.write("<header><h1>");
 document.write(asignatura.nombre);
-document.write("</h1>");
+document.write("</h1></header>");
--- Ejercicio-02/NombreNavegador.js
+++ Ejercicio-02/NombreNavegador.js
@@ -1,6 +1,6 @@
 //NombreNavegador.js
 //Fichero para la tarea 4 del Ejercicio 2
 //de la práctica de ECMAScript de SEW
-document.write("<h1>Información del Navegador: ");
+document.write("<header><h1>Información del Navegador: ");
 document.write(navegador.nombre);
-document.write("</h1>");
+document.write("</h1></header>");
